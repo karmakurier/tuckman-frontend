5122c03dd9eea4ca9cfbcd4788c089427d073501
achiocch <andreas.chiocchetti@web.de>
2021-05-09 09:55:55 +0200

    teamtest component created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
+import { TeamtestComponent } from './teamtest/teamtest.component';
 
 const routes: Routes = [
   { path: 'home', component: HomepageComponent },
+  { path: 'teamtest', component:  TeamtestComponent},
   { path: '**', redirectTo: 'home' }
 ];
 
