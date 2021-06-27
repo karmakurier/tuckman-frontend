80bf24dd4b16ff5456c8cebfb71b15bccc350eb4
achiocch <andreas.chiocchetti@web.de>
2021-06-27 11:19:15 +0200

    added ButtonLinkComponent

---
+++ src/app/components/button-link/button-link.component.ts
@@ -0,0 +1,27 @@
+import { Component, EventEmitter, Input, Output, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-button-link',
+  templateUrl: './button-link.component.html',
+  styleUrls: ['./button-link.component.scss']
+})
+export class ButtonLinkComponent implements OnInit {
+
+  @Input() link: string = "this is the link";
+  @Output() link2: EventEmitter<string>=new EventEmitter<string>();
+
+  constructor() { }
+
+  copyLink(inputElement){
+    inputElement.select();
+    document.execCommand("copy");
+    window.alert("Folgender Link wurde erfolgreich in den  Zwischenspeicher kopiert:  \n\n"+ 
+                  inputElement.value + "\n\nDrücken Sie STRG+V um den Link einzufügen");
+    inputElement.setSelectionRange(0,0);
+    this.link2.next("blabala");
+  }
+
+  ngOnInit(): void {
+  }
+
+}
