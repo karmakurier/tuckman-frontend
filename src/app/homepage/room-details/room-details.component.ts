00c4bff8d98684dd309667b62765c7e2929e3a03
bastisk <basti.sk@gmx.de>
2021-10-11 22:40:01 +0200

    redesign and mail button for room details

--- src/app/homepage/room-details/room-details.component.ts
+++ src/app/homepage/room-details/room-details.component.ts
@@ -9,7 +9,7 @@ import { QuestionnaireResult, QuestionnaireresultService, Room, RoomsService } f
 })
 export class RoomDetailsComponent implements OnInit {
 
-  @Input() teamid: string="AwsomeTeam2";
+  @Input() teamid: string = "AwsomeTeam2";
   accordionExpanded: number = -1;
   room: Room = {} as Room;
   questionnaireResults: QuestionnaireResult[] = [];
@@ -17,28 +17,28 @@ export class RoomDetailsComponent implements OnInit {
   constructor(private activatedRoute: ActivatedRoute, private roomService: RoomsService, private questionnaireResultService: QuestionnaireresultService) { }
 
   expandAccordion(num: number) {
-    if(num === this.accordionExpanded) {
+    if (num === this.accordionExpanded) {
       this.accordionExpanded = -1;
     } else {
       this.accordionExpanded = num;
     }
   }
 
-  oncopy(x:string){
+  oncopy(x: string) {
     console.log("printed")
     console.log(x);
   }
 
-  exportPDF(teamid:string){
-    console.log("pdf export triggered for "+teamid)
+  exportPDF(teamid: string) {
+    console.log("pdf export triggered for " + teamid)
   }
 
-  printPDF(teamid:string){
-    console.log("pdf print triggered for "+teamid)
+  printPDF(teamid: string) {
+    console.log("pdf print triggered for " + teamid)
   }
 
-  share(teamid:string){
-    console.log("social media share triggered for "+teamid)
+  share(teamid: string) {
+    console.log("social media share triggered for " + teamid)
   }
 
   ngOnInit(): void {
@@ -52,7 +52,7 @@ export class RoomDetailsComponent implements OnInit {
 
   hasExpiry() {
     let datestring = new Date(this.room.expiresAt);
-    if( datestring > new Date()) {
+    if (datestring > new Date()) {
       return true;
     }
     return false;
@@ -65,6 +65,10 @@ export class RoomDetailsComponent implements OnInit {
 
   getLinkForResults() {
     return window.location;
+  }
+
+  generateMailToForParticipants() {
+    return "mailto:?subject=Einladung%20zur%20Tuckman-Analyse%20%7C%20karmakurier&body=Du wurdest zu einer Tuckman Analyse eingeladen!%0A%0AFolge diesem Link um teilzunehmen: %0A%0A" + this.getLinkForParticipants()
   }
 
 }
